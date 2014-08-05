533255cff73cbf2cdffc6dbf7924750fd07b9639
Andy <boruil@cs.washington.edu>
2014-08-04 23:12:57 -0700

    send request to personal website

--- script.js
+++ script.js
@@ -1,2 +1,5 @@
 var xhr = new XMLHttpRequest();
-xhr.open("GET", "http://www.codecademy.com/", false);
+xhr.open("GET", "http://www.boruili.com/", false);
+xhr.send();
+console.log(xhr.status);
+console.log(xhr.statusText);
