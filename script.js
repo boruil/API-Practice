5bac4dc145cacc9a3e655d63386475084cd403cb
Andy <boruil@cs.washington.edu>
2014-08-04 23:23:43 -0700

    when js library is loaded

--- script.js
+++ script.js
@@ -10,3 +10,7 @@ function showResponse(response) {
     var responseString = JSON.stringify(response, '', 2);
     document.getElementById('response').innerHTML += responseString;
 }
+
+function onClientLoad() {
+    gapi.client.load('youtube', 'v3', onYouTubeApiLoad);
+}
